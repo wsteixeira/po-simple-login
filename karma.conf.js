378364573d8622a14079bc287265e5b74e890f67
Wanderley Santos Teixeira <wsteixeira@yahoo.com.br>
2022-09-04 21:30:05 -0300

    feat: atera no do projeto

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/po-simple-crud'),
+      dir: require('path').join(__dirname, './coverage/po-simple-login'),
       subdir: '.',
       reporters: [
         { type: 'html' },
